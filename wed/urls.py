62239d20c7d1ecdfb3738387e93bf9d5ae48c156
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-22 22:54:19 +0300

    add search template

--- wed/urls.py
+++ wed/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
   path('', views.index, name='index'),
+  path('gallery/',views.gallery ,name= 'gallery'),
   path("profile/", views.profile, name="profile"),
   path("accounts/profile/", views.profile, name="profile"),
   path("profile/update/", views.update_profile, name="update_profile"),
