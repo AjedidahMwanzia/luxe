f9178f63bd6b7867afcfaefd1d7023ae2a32c8dc
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-22 08:40:17 +0300

    configure static files

--- wed/views.py
+++ wed/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render,redirect
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 def index(request):
