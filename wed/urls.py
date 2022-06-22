49fb58d56edf5661d64583e65d647b77b0b93d55
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-22 22:52:45 +0300

    add search template

--- wed/urls.py
+++ wed/urls.py
@@ -8,6 +8,9 @@ urlpatterns=[
   path("profile/", views.profile, name="profile"),
   path("accounts/profile/", views.profile, name="profile"),
   path("profile/update/", views.update_profile, name="update_profile"),
+  path('photo/<str:pk>',views.viewPhoto ,name= 'photo'),
+  path('search/', views.search_results, name='search_results'),
+  url(r'^location/(\d+)', views.get_location, name='get_location')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
