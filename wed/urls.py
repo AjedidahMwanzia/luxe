0fe526a0600d6c444f15db9a2d9569789cf9a9bf
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-22 22:25:38 +0300

    add rest api

--- wed/urls.py
+++ wed/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns=[
   path('', views.index, name='index'),
+  path('api/profile/', views.ProfileList.as_view()),
+  path('api/project/', views.ProjectList.as_view()),
+  path("profile/", views.profile, name="profile"),
+  path("accounts/profile/", views.profile, name="profile"),
+  path("profile/update/", views.update_profile, name="update_profile"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- wedding/settings.py
+++ wedding/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'cloudinary',
     'bootstrap3',
     'registration',
+    'rest_framework',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
